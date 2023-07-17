fa252e2a111fe7ab5686f651ec1e917713886ee0
mariacabriles29 <mecp2992@gmail.com>
2023-07-17 10:38:55 -0400

    LazyLoad Basico Listo

--- src/01-lazyload/pages/LazyPage1.tsx
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -3,7 +3,8 @@ import React from "react";
 export const LazyPage1 = () => {
   return (
     <>
-      <h1>LazyPage 1</h1>;<h2>Test Lazy 1</h2>
+      <h1>LazyPage 1</h1>
     </>
   );
 };
+export default LazyPage1;
---
+++ src/routes/routes.ts
@@ -0,0 +1,45 @@
+/*archivo para crear rutas independientes rutas independientes  */
+import { LazyExoticComponent, lazy } from "react";
+
+type JSXComponent = () => JSX.Element;
+interface Route {
+  to: string;
+  path: string;
+  name: string;
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent;
+}
+
+/*Creacion de un LazyLoad suspense, al agregar el  webpackChunkName se le colocan nombes a los chunck de cada pagina   */
+const Lazy1 = lazy(
+  () =>
+    import(/* webpackChunkName: "LazyPage1" */ "../01-lazyload/pages/LazyPage1")
+);
+const Lazy2 = lazy(
+  () =>
+    import(/* webpackChunkName: "LazyPage2" */ "../01-lazyload/pages/LazyPage2")
+);
+const Lazy3 = lazy(
+  () =>
+    import(/* webpackChunkName: "LazyPage3" */ "../01-lazyload/pages/LazyPage3")
+);
+
+export const routes: Route[] = [
+  {
+    to: "/lazy1",
+    path: "lazy1",
+    Component: Lazy1,
+    name: "Lazy-1",
+  },
+  {
+    to: "/lazy2",
+    path: "lazy2",
+    Component: Lazy2,
+    name: "Lazy-2",
+  },
+  {
+    to: "/lazy3",
+    path: "lazy3",
+    Component: Lazy3,
+    name: "Lazy-3",
+  },
+];
