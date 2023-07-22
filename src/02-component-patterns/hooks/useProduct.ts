a3b0fe1c96b26375b47376e41a1f80dda844b034
mariacabriles29 <mecp2992@gmail.com>
2023-07-22 15:29:43 -0400

    control props alternativo y mas facil de entender

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Product, onChangeArgs } from "../interfaces/interfaces";
 
 interface useProductArgs {
@@ -13,13 +13,8 @@ export const useProduct = ({
 }: useProductArgs) => {
   const [counter, setCounter] = useState(value);
 
-  const isControlled = useRef(!!onChange);
-
   /* con esta funcion incremento y decremento los valores de los productos */
   const increaseBy = (value: number) => {
-    if (isControlled.current) {
-      return onChange!({ count: value, product });
-    }
     const newValue = Math.max(counter + value, 0);
     setCounter(newValue);
     // con esta condicion && confirmo que onChange tiene algun valor
