2634c9861292d5d1ec4f71205f2d8ba9a362434d
mariacabriles29 <mecp2992@gmail.com>
2023-07-26 11:37:49 -0400

    feat: :art: Acualizacion de los inputs y toma de los valores del formulario

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,43 +1,55 @@
 import "../styles/styles.css";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 export const RegisterPage = () => {
   const [registerData, setRegisterData] = useState({
-    name: "maria",
-    email: "mariac29@gmail.com",
-    password1: "123456",
-    password2: "123456",
+    name: "",
+    email: "",
+    password1: "",
+    password2: "",
   });
   /*desestructuro lo que necesito */
 
   const { name, email, password1, password2 } = registerData;
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value);
+    setRegisterData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(registerData);
   };
   return (
     <div>
       <h1>Register Page</h1>
-      <form>
+      <form noValidate onSubmit={onSubmit}>
         <input
           type="text"
           placeholder="Name"
+          name="name"
           value={name}
           onChange={onChange}
         />
         <input
           type="email"
           placeholder="Email"
+          name="email"
           value={email}
           onChange={onChange}
         />
         <input
           type="password"
           placeholder="Password"
+          name="password1"
           value={password1}
           onChange={onChange}
         />
         <input
           type="password"
           placeholder=" Repeat Password"
+          name="password2"
           value={password2}
           onChange={onChange}
         />
